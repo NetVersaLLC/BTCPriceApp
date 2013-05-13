758d68d0583c619723a62f6ee40bf0eb1a2c51a4
Michael Shick <mike@shick.in>
2013-05-12 23:53:00 -0400

    Added dummy candlestick chart view

---
+++ src/com/netversa/btcprice/CandlestickChartView.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2013 NetVersa LLC
+ */
+package com.netversa.btcprice;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CandlestickChartView extends View
+{
+    public CandlestickChartView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+}
