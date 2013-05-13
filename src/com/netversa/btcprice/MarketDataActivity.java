381e8fec78b4b6c622f3f7503cd6bf25ec28c463
Michael Shick <mike@shick.in>
2013-05-12 21:43:51 -0400

    Android skeleton build

---
+++ src/com/netversa/btcprice/MarketDataActivity.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2013 NetVersa LLC
+ */
+package com.netversa.btcprice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MarketDataActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
