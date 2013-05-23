9fd5182eb621062e8a8549ae221e9872320cfe17
Michael Shick <mike@shick.in>
2013-05-22 23:46:42 -0400

    Initial MarketDataActivity tests
    working rotation and data persistence test
    broken data reception test: must not touch views from outside activity (use a
    handler?)

--- tests/src/com/netversa/btcprice/MarketDataActivityTest.java
+++ tests/src/com/netversa/btcprice/MarketDataActivityTest.java
@@ -1,5 +1,7 @@
 package com.netversa.btcprice;
 
+import android.content.pm.ActivityInfo;
+
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -19,6 +21,9 @@ public class MarketDataActivityTest extends ActivityInstrumentationTestCase2<Mar
 {
     protected MarketDataActivity activity;
     protected MarketData marketData;
+    protected MarketData cachedMarketData;
+    protected String errorString;
+    protected long expectResultsBy;
 
     public MarketDataActivityTest()
     {
@@ -31,15 +36,86 @@ public class MarketDataActivityTest extends ActivityInstrumentationTestCase2<Mar
         super.setUp();
         activity = getActivity();
 
-        marketData = new MarketData("mtgox", "BTC", "USD", new BigDecimal("1.00"),
-                new BigDecimal("0.99"), new BigDecimal("1.01"), new BigDecimal("1.99"),
-                new BigDecimal("0.01"), new BigDecimal("100.00"), new Date(1369196546000l));
+        errorString = "derp";
+        marketData = new MarketData("mtgox", "BTC", "USD",
+                new BigDecimal("1.00"), new BigDecimal("0.99"),
+                new BigDecimal("1.01"), new BigDecimal("1.99"),
+                new BigDecimal("0.01"), new BigDecimal("100.00"),
+                new Date(1369196546000l));
+        cachedMarketData = new MarketData("mtgox", "BTC", "USD",
+                new BigDecimal("1.00"), new BigDecimal("0.99"),
+                new BigDecimal("1.01"), new BigDecimal("1.99"),
+                new BigDecimal("0.01"), new BigDecimal("100.00"),
+                new Date(1369196946000l));
+        expectResultsBy = 1369197946000l;
 
         activity.marketData = marketData;
+        activity.cachedMarketData = cachedMarketData;
+        activity.errorString = errorString;
+        activity.expectResultsBy = expectResultsBy;
     }
 
     public void testRotation()
     {
+        activity.setRequestedOrientation(
+                ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        assertEquals("marketData preservation",
+                activity.marketData, marketData);
+        assertEquals("cachedMarketData preservation",
+                activity.cachedMarketData, cachedMarketData);
+        assertEquals("errorString preservation",
+                activity.errorString, errorString);
+        assertEquals("expectResultsBy preservation",
+                activity.expectResultsBy, expectResultsBy);
+
+        activity.setRequestedOrientation(
+                ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        // clean up just in case
+        activity.marketData = marketData;
+        activity.cachedMarketData = cachedMarketData;
+        activity.errorString = errorString;
+        activity.expectResultsBy = expectResultsBy;
     }
+
+    public void testDataDisplay()
+    {
+        // FIXME gotta run this on the activity's own thread
+        activity.completeRefresh();
+
+        String expectedErrorText = errorString;
+        String expectedPriceText =
+            String.format(activity.getString(R.string.price_format),
+                    cachedMarketData.lastPrice);
+        String expectedCurrencyText =
+            String.format(activity.getString(R.string.currency_pair_format),
+                    cachedMarketData.baseCurrency,
+                    cachedMarketData.counterCurrency);
+        String expectedHighText =
+            String.format(activity.getString(R.string.high_price_format),
+                    cachedMarketData.highPrice);
+        String expectedLowText =
+            String.format(activity.getString(R.string.low_price_format),
+                    cachedMarketData.lowPrice);
+        String expectedVolumeText =
+            String.format(activity.getString(R.string.volume_format),
+                    cachedMarketData.volume);
+
+        assertEquals("error readout correctness",
+                activity.errorView.getText(), expectedErrorText);
+        assertEquals("price readout correctness",
+                activity.priceView.getText(), expectedPriceText);
+        assertEquals("currency readout correctness",
+                activity.currencyView.getText(), expectedCurrencyText);
+        assertEquals("high price readout correctness",
+                activity.highPriceView.getText(), expectedHighText);
+        assertEquals("low price readout correctness",
+                activity.lowPriceView.getText(), expectedLowText);
+        assertEquals("volume readout correctness",
+                activity.volumeView.getText(), expectedVolumeText);
+    }
+
+    // TODO learn up on injecting intents and test broadcastreceiver
 
 }
