5305dea0ec5b6318fb48d3f79ea2a9b1e160f406
Michael Shick <mike@shick.in>
2013-05-27 20:23:51 -0400

    first nontrivial FetchService unit test

--- tests/src/com/netversa/btcprice/FetchServiceTest.java
+++ tests/src/com/netversa/btcprice/FetchServiceTest.java
@@ -3,19 +3,54 @@
  */
 package com.netversa.btcprice;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
 import android.test.ServiceTestCase;
 
 public class FetchServiceTest extends ServiceTestCase<FetchService>
 {
+    protected IntentFilter intentFilter;
+    protected Context ctx;
+
     public FetchServiceTest()
     {
         super(FetchService.class);
+
+        intentFilter = new IntentFilter(FetchService.ACTION_RESPONSE);
+        intentFilter.addDataScheme(FetchService.DATA_SCHEME);
     }
 
-    // TODO setUp that puts service in testing mode and injects mock exchange
-    // objects
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+
+        ctx = getContext();
+    }
 
     public void testNullTarget() throws Throwable
     {
+        startService(new Intent(FetchService.ACTION_REQUEST));
+    }
+
+    public void testNullExchange() throws Throwable
+    {
+        String badTarget = "data:///market/USD/BTC";
+        String format = ctx.getString(R.string.fetch_error_bad_target_format);
+        final String expectedError = String.format(format, badTarget);
+
+        ctx.registerReceiver(new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                assertEquals("null-exchange market data", null,
+                    intent.getParcelableExtra(FetchService.EXTRA_MARKET_DATA));
+                assertEquals("null-exchange error string", expectedError,
+                    intent.getStringExtra(FetchService.EXTRA_ERROR_STRING));
+            }
+        }, intentFilter);
+
+        startService(new Intent(FetchService.ACTION_REQUEST,
+                Uri.parse(badTarget)));
     }
 }
