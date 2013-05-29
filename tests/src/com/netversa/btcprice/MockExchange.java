7af5fd9f1dd236588ab9f7da6788eb66756ab903
Michael Shick <mike@shick.in>
2013-05-28 21:14:49 -0400

    stubbed MockExchange

---
+++ tests/src/com/netversa/btcprice/MockExchange.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2013 NetVersa LLC
+ */
+package com.netversa.btcprice;
+
+import com.xeiam.xchange.Exchange;
+import com.xeiam.xchange.ExchangeSpecification;
+import com.xeiam.xchange.service.account.polling.PollingAccountService;
+import com.xeiam.xchange.service.marketdata.polling.PollingMarketDataService;
+import com.xeiam.xchange.service.trade.polling.PollingTradeService;
+import com.xeiam.xchange.service.streaming.ExchangeStreamingConfiguration;
+import com.xeiam.xchange.service.streaming.StreamingExchangeService;
+
+public class MockExchange implements Exchange
+{
+    public ExchangeSpecification getExchangeSpecification()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public ExchangeSpecification getDefaultExchangeSpecification()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public void applySpecification(ExchangeSpecification exchangeSpecification)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public PollingMarketDataService getPollingMarketDataService()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public PollingMarketDataService getPollingMarketDataService(
+            ExchangeStreamingConfiguration configuration)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public StreamingExchangeService getStreamingExchangeService()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public StreamingExchangeService getStreamingExchangeService(
+            ExchangeStreamingConfiguration configuration)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public PollingTradeService getPollingTradeService()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public PollingTradeService getPollingTradeService(
+            ExchangeStreamingConfiguration configuration)
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public PollingAccountService getPollingAccountService()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public PollingAccountService getPollingAccountService(
+            ExchangeStreamingConfiguration configuration)
+    {
+        throw new UnsupportedOperationException();
+    }
+}
