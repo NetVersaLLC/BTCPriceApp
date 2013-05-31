67d204bb329824b4c33adf9e14412b927074b577
Michael Shick <mike@shick.in>
2013-05-30 23:25:18 -0400

    added defaults class for prefs

---
+++ src/com/netversa/btcprice/Defaults.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2013 NetVersa LLC
+ */
+package com.netversa.btcprice;
+
+/** Central storage for default values for SharedPreferences.
+ */
+public class Defaults
+{
+    // 30 seconds in ms
+    public static final int FETCH_TIMEOUT = 30000;
+}
