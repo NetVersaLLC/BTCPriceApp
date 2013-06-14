f2734a859ed65110da105baae8c2e85742c0af60
Michael Shick <mike@shick.in>
2013-06-13 21:17:23 -0400

    start-at-boot receiver stub

---
+++ src/com/netversa/btcprice/StartAtBootReceiver.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2013 NetVersa LLC
+ */
+package com.netversa.btcprice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/** Start services that run constantly as soon as possible when the device
+ * starts.
+ */
+public class StartAtBootReceiver extends BroadcastReceiver
+{
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+    }
+}
