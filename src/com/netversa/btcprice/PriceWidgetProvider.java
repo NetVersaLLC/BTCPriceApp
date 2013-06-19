5e42c830a4d324e87ba32cbae9f5a70501ac9055
Michael Shick <mike@shick.in>
2013-06-18 23:18:33 -0400

    skeletal price widget

---
+++ src/com/netversa/btcprice/PriceWidgetProvider.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2013 NetVersa LLC
+ */
+package com.netversa.btcprice;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class PriceWidgetProvider extends AppWidgetProvider
+{
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+            int[] appWidgetIds) {
+    }
+}
