13b161809c9534140bfaf780691ee094829fa557
Michael Shick <mike@shick.in>
2013-06-19 22:11:24 -0400

    basic price widget working

--- src/com/netversa/btcprice/PriceWidgetProvider.java
+++ src/com/netversa/btcprice/PriceWidgetProvider.java
@@ -6,11 +6,20 @@ package com.netversa.btcprice;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class PriceWidgetProvider extends AppWidgetProvider
 {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
             int[] appWidgetIds) {
+        SharedPreferences prefs =
+            PreferenceManager.getDefaultSharedPreferences(context);
+
+        FetchService.requestMarket(context, null,
+                prefs.getString("def_exchange", Defaults.DEF_EXCHANGE),
+                prefs.getString("def_base", Defaults.DEF_BASE),
+                prefs.getString("def_counter", Defaults.DEF_COUNTER));
     }
 }
