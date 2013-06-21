c5b071368af466eb4ad5f98da29d6df19f77f408
Michael Shick <mike@shick.in>
2013-06-20 21:33:39 -0400

    clicking on price widget while loading opens program

--- src/com/netversa/btcprice/PriceWidgetProvider.java
+++ src/com/netversa/btcprice/PriceWidgetProvider.java
@@ -3,17 +3,39 @@
  */
 package com.netversa.btcprice;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.widget.RemoteViews;
 
+/** Manage lifecycle and updating of numeric price widgets other than loading
+ * them with fetched data.
+ */
 public class PriceWidgetProvider extends AppWidgetProvider
 {
+    /** When a widget is first placed or otherwise updated, put up a placeholder
+     * graphic and request a market data fetch.
+     */
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
             int[] appWidgetIds) {
+        RemoteViews widgetViews = new RemoteViews(context.getPackageName(),
+                R.layout.price_widget_loading);
+
+        PendingIntent wIntent = PendingIntent.getActivity(context, 0,
+                context.getPackageManager().getLaunchIntentForPackage(
+                    context.getPackageName()), 0);
+
+        widgetViews.setOnClickPendingIntent(R.id.widget, wIntent);
+
+        for(int widgetId : appWidgetIds)
+        {
+            appWidgetManager.updateAppWidget(widgetId, widgetViews);
+        }
+
         SharedPreferences prefs =
             PreferenceManager.getDefaultSharedPreferences(context);
 
